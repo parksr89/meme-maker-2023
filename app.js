c2f350101f9ae655fc62e43ec74a632be8cfa347
parksr89 <54193969+parksr89@users.noreply.github.com>
2023-09-28 20:42:08 -0700

    update

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const color =document.getElementById("color");
 const lineWidth =document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx =canvas.getContext("2d");
+const elseColor = document.getElementById("else-color");
+const lineWidthDisplay =document.querySelector("#line-width-display");
+const lineWidthBtnThin =document.querySelector("#line-width-thinbtn");
+const lineWidthBtnBold =document.querySelector("#line-width-boldbtn");
+const textModeBtn = document.querySelector("#text-mode-btn");
+const fontSizeInput = document.querySelector("#font-size-input");
+
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 
@@ -17,9 +24,11 @@ canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
 
+
+let isTextTransfroming =false;
 let isPainting = false;
 let isFilling = false;
-
+let fontSize = 100; 
 
 function onMove(event) {
     if (isPainting){
@@ -39,8 +48,29 @@ function cancelPainting(){
     isPainting =false;
 }
 
-function onLineWidthChange(event){    
+function onLineWidthChange(event){       
     ctx.lineWidth = event.target.value;
+    lineWidthDisplay.innerText = ctx.lineWidth;
+}
+
+function onLineWidthBtnClick(event){
+
+    const lineWidthBtn = ctx.lineWidth;
+
+    if(lineWidthBtn > 0){
+        if(event.target.textContent === "Thin"){            
+            ctx.lineWidth = lineWidthBtn  - event.detail*0.5; 
+        }
+    }
+
+    if(lineWidthBtn < 20){   
+        if(event.target.textContent === "Bold"){        
+            ctx.lineWidth = lineWidthBtn  + event.detail*0.5;
+        }
+    }
+
+    lineWidthDisplay.innerText = ctx.lineWidth; 
+    lineWidth.value = ctx.lineWidth;   
 }
 
 function strokeStyleAndfillStyle(event){
@@ -56,13 +86,14 @@ function onColorClick(event){
     const colorValue = event.target.dataset.color;
     strokeStyleAndfillStyle(colorValue);
     color.value = colorValue;
+    elseColor.style.backgroundColor = color.value;    
 }
 
 function onModeClick(){    
 
     if(isFilling){
         isFilling = false;
-        modeBtn.innerText ="Fill";
+        modeBtn.innerText ="🩸 Fill";
     } else{
         isFilling = true;
         modeBtn.innerText ="Draw";
@@ -80,7 +111,7 @@ function onDestroyClick(){
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-function omEraserClick(){
+function onEraserClick(){
     ctx.strokeStyle ="white";
     isFilling = false;
     modeBtn.innerText ="Fill";
@@ -97,16 +128,43 @@ function onFileChange(event){
     }
 }
 
+function fontSizeChange(event){
+    if( fontSize !== parseInt(event.target.value)){
+        fontSize = parseInt(event.target.value);
+    };       
+}
+
 function textStamp(event){
-    const text = textInput.value;
+    const text = textInput.value;  
     if(text !==""){
         ctx.save();
-        ctx.lineWidth = 1;
-        ctx.font = "48px serif";
-        ctx.fillText(text,event.offsetX,event.offsetY);
+        ctx.lineWidth = 1; 
+        /* ctx.rotate((1 * Math.PI) / 180);  */            
+        ctx.font = `${fontSize}px serif`;  
+        
+        if (isTextTransfroming){
+            ctx.fillText(text,event.offsetX,event.offsetY);
+        }else{        
+            ctx.strokeText(text,event.offsetX,event.offsetY);
+        }
+        console.log(event.offsetX,event.offsetY);
         ctx.restore();
     }
 }
+
+
+function ontextModeClick(){
+    
+    if(isTextTransfroming){
+        isTextTransfroming = false;
+        textModeBtn.innerText ="Stroke Text";
+    }else{
+        isTextTransfroming = true;
+        textModeBtn.innerText ="Fill Text";
+    }
+
+}
+
 
 function onSaveClick(event){
     const url = (canvas.toDataURL());
@@ -125,10 +183,15 @@ canvas.addEventListener("mouseup",cancelPainting);
 canvas.addEventListener("mouseleave",cancelPainting);
 canvas.addEventListener("click",onCanvasFillingClick);
 lineWidth.addEventListener("change", onLineWidthChange);
+lineWidthBtnThin.addEventListener("click",onLineWidthBtnClick);
+lineWidthBtnBold.addEventListener("click",onLineWidthBtnClick);
 color.addEventListener("change",onColorChange);
 colorOption.forEach(color => color.addEventListener("click",onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destoryBtn.addEventListener("click", onDestroyClick);
-eraserBtn.addEventListener("click", omEraserClick);
+eraserBtn.addEventListener("click", onEraserClick);
 fileInput.addEventListener("change",onFileChange);
-saveBtn.addEventListener("click",onSaveClick);
+saveBtn.addEventListener("click", onSaveClick);
+textModeBtn.addEventListener("click", ontextModeClick);
+fontSizeInput.addEventListener("change", fontSizeChange);
+
